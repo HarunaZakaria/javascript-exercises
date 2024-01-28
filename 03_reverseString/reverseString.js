4f033f7ac3e2fc7831ca142e2d2137adf2ef2f9e
harunazakaria <Harunzy55@gmail.com.com>
2024-01-28 19:49:19 +0000

    add solution to the 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  let reversedString = '';
+  for (let i = input.length - 1; i >= 0; i--) {
+    reversedString += input[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+/* function reverseString(input) {
+    let reversedString = "";
+    for (let i = input.length - 1; i >= 0; i--) {
+      reversedString += input[i];
+    }
+    return reversedString;
+  } */
