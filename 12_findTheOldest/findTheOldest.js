c4bbb828927f237aab532c50b3575f5965a9f3c6
harunazakaria <Harunzy55@gmail.com.com>
2024-03-11 09:40:54 +0000

    add all the solutions to the tasks

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPerson = null;
+  let maxAge = 0;
 
+  for (let person of people) {
+    let age = person.deathYear - person.birthYear;
+    if (age > maxAge) {
+      maxAge = age;
+      oldestPerson = person;
+    }
+  }
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
